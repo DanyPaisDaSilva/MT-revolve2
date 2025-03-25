ce37ef1cc18cba2e7381bcb379b8e88ac1737161
DanyPaisDaSilva <danypaisdasilva@gmail.com>
2025-03-25 14:34:24 +0100

    added new stepwise simulator to revolve2

--- simulators/mujoco_simulator/revolve2/simulators/mujoco_simulator/__init__.py
+++ simulators/mujoco_simulator/revolve2/simulators/mujoco_simulator/__init__.py
@@ -1,5 +1,6 @@
 """Physics simulator using the MuJoCo."""
 
 from ._local_simulator import LocalSimulator
+from ._stepwise_simulator import StepwiseSimulator
 
-__all__ = ["LocalSimulator"]
+__all__ = ["LocalSimulator", "StepwiseSimulator"]
